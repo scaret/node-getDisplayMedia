085acd8ca6caab80377083d7323e76623b18b22a
Derek Lu <scaret.in@gmail.com>
2019-07-01 14:47:59 +0800

    Removed wrtc dependency(by passing the whole library when the module is required

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Supports:
 - OSX
 
 ```JavaScript
-const getDisplayMedia = require("getdisplaymedia");
+const wrtc = require("wrtc");
+const getDisplayMedia = require("getdisplaymedia")(wrtc);
 getDisplayMedia().then((ms)=>{
     ms.getVideoTracks().forEach((track, index){
         console.log(`Track ${index}, kind ${track.kind}, id ${track.id}`)
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const fs = require("fs");
-const wrtc = require("wrtc");
 
-function getDisplayMedia(options){
-    const platform = process.platform;
-    const capturer = require(`./capture/${platform}/capture`)(options || {});
-    const source = new wrtc.nonstandard.RTCVideoSource();
-    const track = source.createTrack();
-    const stream = new wrtc.MediaStream();
-    stream.addTrack(track);
+module.exports = (wrtc)=>{
+    const getDisplayMedia = (options)=>{
+        const platform = process.platform;
+        const capturer = require(`./capture/${platform}/capture`)(options || {});
+        const source = new wrtc.nonstandard.RTCVideoSource();
+        const track = source.createTrack();
+        const stream = new wrtc.MediaStream();
+        stream.addTrack(track);
 
-    capturer.start();
-    capturer.on("frame", (frame)=>{
-        if (track.readyState === "ended"){
-            console.log("video track ended. Stopping screensharing.");
-            capturer.stop();
-        }else{
-            source.onFrame(frame);
-        }
-    });
+        capturer.start();
+        capturer.on("frame", (frame)=>{
+            if (track.readyState === "ended"){
+                console.log("video track ended. Stopping screensharing.");
+                capturer.stop();
+            }else{
+                source.onFrame(frame);
+            }
+        });
 
-    return Promise.resolve(stream);
-}
-
-module.exports = getDisplayMedia;
+        return Promise.resolve(stream);
+    };
+    return getDisplayMedia;
+};
 
 // getDisplayMedia().then((ms)=>{
 //    console.log("MediaStream", ms);
